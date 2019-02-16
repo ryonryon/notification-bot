e287601896c1b1b7e3080a4a316a21605837d5c2
ryotogashi <ryo.togashi.ca@gmail.com>
2019-02-15 20:56:58 -0800

    Edit index.js and add notificationList.json
    still todo which is channel id should be channel name is remain.

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
-const Botkit = require('botkit');
+// TODO: do not import packages in the middle of the code
+// TODO: use camel case instead
+// TODO: add an additional line before return statements
+// TODO: use `` syntax instead of string concatnation (e.g. "foo" + text -> `foo${text}`)
+// TODO: format the code. do not leave meaningless extra lines
+// TODO: do not use process.exit. create main() function to avoid running the code instead
+// TODO: use arrow function syntax () => instead of normal function syntax as callback functions instead
+// TODO: use for-of statement syntax instead
+// TODO: use Array#includes() instead
 
-if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
-}
+const botkit = require("botkit");
+const fs = require("fs");
 
-const controller = Botkit.slackbot({
+const jsonFile = "./notificationList.json";
+const UTF8 = "utf8";
+
+const createMessage = (word, channel, text, url) =>
+  `Listed word( ${word} ) is posted in channel( ${channel} ) \n content: ${text} \n message url is <${url}> \n`;
+
+const createUrl = (siteUrl, channelId, eventTs) =>
+  `${siteUrl}/archives/${channelId}/${eventTs}`;
+
+const main = () => {
+  const json = JSON.parse(fs.readFileSync(jsonFile, UTF8));
+
+  const controller = botkit.slackbot({
     debug: false
-});
+  });
 
-controller.spawn({
-    token: process.env.token
-}).startRTM(function(err){
-    if (err) {
+  controller
+    .spawn({
+      token: json.token
+    })
+    .startRTM(function(err) {
+      if (err) {
         throw new Error(err);
-    }
-});
+      }
+    });
 
-// say hi
-controller.hears('hi',['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,'hi');
-});
+  controller.on("ambient", function(bot, message) {
+    const post = message["text"].toLowerCase();
+    for (let listedWord of json.list) {
+      if (!post.includes(listedWord)) {
+        continue;
+      }
+
+      const msgUrl = createUrl(
+        json.slackUrl,
+        message["channel"],
+        message["event_ts"].replace(".", "")
+      );
+
+      // TODO: msgChannel should be name not id
+      const textContent = createMessage(
+        listedWord,
+        message["channel"],
+        message["text"],
+        msgUrl
+      );
+
+      bot.say({
+        channel: json.msgRecieveUserId,
+        text: textContent
+      });
+    }
+  });
+};
+
+main();
