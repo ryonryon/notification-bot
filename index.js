181f2803610911d9e56730c791b66257653d99d5
ryotogashi <ryo.togashi.ca@gmail.com>
2019-02-16 16:55:47 -0800

    Add try catch.

--- index.js
+++ index.js
@@ -5,55 +5,72 @@ const jsonFile = "./notificationList.json";
 const UTF8 = "utf8";
 
 const createMessage = (word, channel, text, url) =>
-  `Listed word( ${word} ) is posted in channel( ${channel} ) \n content: ${text} \n message url is <${url}> \n`;
+  `Listed word( ${word} ) is posted in channel( ${channel} ) \n content: \n ${text} \n message url is <${url}> \n`;
 
 const createUrl = (siteUrl, channelId, eventTs) =>
   `${siteUrl}/archives/${channelId}/${eventTs}`;
 
 const main = () => {
-  const json = JSON.parse(fs.readFileSync(jsonFile, UTF8));
-
-  const controller = botkit.slackbot({
-    debug: false
-  });
-
-  controller
-    .spawn({
-      token: json.token
-    })
-    .startRTM(function(err) {
-      if (err) {
-        throw new Error(err);
+  try{
+    if (!fs.existsSync(jsonFile)){
+      throw 91;
+    }
+    const json = JSON.parse(fs.readFileSync(jsonFile, UTF8));
+  
+    const controller = botkit.slackbot({
+      debug: false
+    });
+  
+    controller
+      .spawn({
+        token: json.token
+      })
+      .startRTM(function(err) {
+        if (err) {
+          throw new Error(err);
+        }
+      });
+  
+    controller.on("ambient", function(bot, message) {
+      const post = message["text"].toLowerCase();
+      for (let listedWord of json.list) {
+        if (!post.includes(listedWord)) {
+          continue;
+        }
+  
+        const msgUrl = createUrl(
+          json.slackUrl,
+          message["channel"],
+          message["event_ts"].replace(".", "")
+        );
+  
+        // TODO: msgChannel should be name not id
+        const textContent = createMessage(
+          listedWord,
+          message["channel"],
+          message["text"],
+          msgUrl
+        );
+  
+        bot.say({
+          channel: json.msgRecieveUserId,
+          text: textContent
+        });
       }
     });
+  
+    return 0;
 
-  controller.on("ambient", function(bot, message) {
-    const post = message["text"].toLowerCase();
-    for (let listedWord of json.list) {
-      if (!post.includes(listedWord)) {
-        continue;
-      }
+  } catch(err) {
 
-      const msgUrl = createUrl(
-        json.slackUrl,
-        message["channel"],
-        message["event_ts"].replace(".", "")
-      );
+    if (err === 91) {
+      console.log(`Path (${jsonFile}) is not exist.`);
+    } else {
+      console.log(err);
+    }; 
 
-      // TODO: msgChannel should be name not id
-      const textContent = createMessage(
-        listedWord,
-        message["channel"],
-        message["text"],
-        msgUrl
-      );
+    return 1;
 
-      bot.say({
-        channel: json.msgRecieveUserId,
-        text: textContent
-      });
-    }
-  });
+  }
 };
-
 main();
